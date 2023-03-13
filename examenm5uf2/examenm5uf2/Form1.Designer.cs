935ce81ef2ed6b4a56c5840c54dd87f13a76ccf4
Luis Falzo <luisalberto.falzoc@educem.net>
2023-03-13 08:58:31 +0100

    modificacion .gitignore

--- examenm5uf2/examenm5uf2/Form1.Designer.cs
+++ examenm5uf2/examenm5uf2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace examenm5uf2
 {
-	partial class Form1
+	partial class FrmMain
 	{
 		/// <summary>
 		/// Variable del diseñador necesaria.
@@ -28,13 +28,47 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Yellow;
+			this.button1.Location = new System.Drawing.Point(61, 79);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(288, 53);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "&Animal";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Fuchsia;
+			this.button2.Location = new System.Drawing.Point(392, 79);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(308, 53);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "&Viaje";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// FrmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "FrmMain";
+			this.Text = "FrmMain";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
 	}
 }
 
